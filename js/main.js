61e44369fef200a7d1b18b25494db0c7e029f4cf
MangoY <119995977+Park-Piao@users.noreply.github.com>
2026-02-01 12:50:02 +1300

    "Initial commit - basic static site"

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const app = document.getElementById("app");
+
+const content = `
+    <h1>This is a MockTesting website.</h1>
+    <p>And it's heckering perfect.</p>
+
+    <h2>Seriously, what the heck else do you want?</h2>
+    
+    <p>you probably build websites and think your idea is special.</p>
+
+    <ul>
+        <li>Shit's lightweight and loads fast</li>
+        <li>Fits on all your shitty screens</li>
+        <li>Looks the same in all your shitty browsers</li>
+        <li>The motherheckerer's accessible to every asshole that visits your site</li>
+        <li>Shit's legible and gets your heckering point across</li>
+    
+    </ul>
+
+    <h3>Well guess what, TestMocker:</h3>
+
+    <p>You. Are. Over-designing. Look at this shit. It's a motherheckering website. Why the hecker do you need to animate a heckering trendy-ass banner flag when I hover over that useless piece of shit? You spent hours on it and added 80 kilobytes to your heckering site, and some motherheckerer jabbing at it on their iPad with fat sausage fingers will never that see shit. Not to mention blind people will never see that shit, but they don't see any of your shitty shit.</p>
+    <p>You never knew it, but this is your perfect mock test website.</P>
+
+    <h2>This is really lightweight!</h2>
+    <P>This entire page weighs less than the gradient-meshed facebook logo on your heckering Wordpress site. Did you seriously load 100kb of jQuery UI just so you could animate the heckering background color of a div? You loaded all 7 fontfaces of a shitty webfont just so you could say "Hi." at 100px height at the beginning of your site? You piece of shit.</p>
+
+    <h2>It's reponsive</h2>
+    <p>You dumbass. You thought you needed media queries to be responsive, but no. Responsive means that it responds to whatever motherheckering screensize it's viewed on. This site doesn't care if you're on an iMac or a motherheckering Tamagotchi.</p>
+
+    <h2>This really works</h2>
+    <p>Look at this shit. You can read it . That is, if you can read, motherheckerer. It makes sense. It has motherheckering hierarchy. It's using HTML5 tags so you and your bitch-ass browser know what the hecker's in this heckering site. That's semantics, motherheckerer.</p>
+    <p>It has content on the heckering page. Your site has three bylines and link to your dribbble account, but you spread it over seven full screens and make me click some heckering button to show the content. Cross-device compatible? That's a heckering bonus.</p>
+
+    <h2>This is a website. Look at it. You've never seen one before</h2>
+    <p>Like the man who's never grown out his beard has no idea what his true natural state is, you have no heckering idea what a website is. All you have ever seen are shitty skeuomorphic bastardizations of what should be text communicating a heckering message. This is a real, naked website. Look at it. It's heckering beautiful.</p>
+
+    <h3>Yes, this is heckering satire, you hecker</h3>
+    <p>I'm not actually saying your shitty site should look like this. What I'm saying is that all the problems we have with websites are <strong>ones we create ourselves</strong>. Websites aren't broken by default, they are functional, high-performing, and accessible. You break them. You son-of-a-bitch.</p>
+
+    <blockquote> 
+        <p>
+        "Good design is as little design as possible."<br>
+        - some German testmocker.
+        </p>
+    </blockquote>
+
+    <p>This is a website. Look at it. It's perfect.</p>
+
+`;  
+
+app.innerHTML = content;
+
